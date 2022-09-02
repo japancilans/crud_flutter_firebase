6ac6f51d7c61d4c8a0ddc39409ab56db3e4b26de
japancilans <japan.cilans@gmail.com>
2022-09-02 12:26:25 +0530

    CRUD DEMO

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,8 +11,6 @@ Future main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,20 +33,47 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    CollectionReference user = FirebaseFirestore.instance.collection('Users');
     return Scaffold(
       appBar: AppBar(
         title: const Text('CRUD DEMO'),
       ),
-      body: const Center(
-        child: Text(
-          'You have pushed the button this many times:',
-        ),
+      body: Center(
+        child: ListView.builder(
+            itemCount: 2,
+            itemBuilder: (BuildContext context, int index) {
+              return FutureBuilder<DocumentSnapshot>(
+                future: user.doc('QpLHQAFdS5MEJadwk7aK').get(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<DocumentSnapshot> snapshot) {
+                  if (snapshot.hasError) {
+                    return const Text("Something went wrong");
+                  }
+                  if (snapshot.hasData && !snapshot.data!.exists) {
+                    return const Text("Document does not exist");
+                  }
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    Map<String, dynamic> data =
+                        snapshot.data!.data() as Map<String, dynamic>;
+                    return Text("Name: ${data['Name']} Age: ${data['Age']}");
+                  }
+                  return const Text("loading");
+                },
+              );
+            }),
       ),
-      floatingActionButton: const FloatingActionButton(
-        onPressed: null,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          adduser();
+        },
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
     );
+  }
+
+  Future<void> adduser() {
+    CollectionReference user = FirebaseFirestore.instance.collection('Users');
+    return user.add({'Name': 'japan', 'Age': 24});
   }
 }
